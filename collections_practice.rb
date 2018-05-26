a36d5836a42e983c0db20556c595d99cf804ed05
Leticia Molina <letimolina@gmail.com>
2018-05-26 22:49:44 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,16 +8,6 @@ def sort_array_desc(array)
   arr2
 end
 
-def sort_array_asc(array)
-  array.sort
-end
-
-def sort_array_desc(array)
-  array.sort do | left, right|
-    right <=> left
-  end
-end
-
 def sort_array_char_count(array)
   array.sort do |left, right|
     left.length <=> right.length
